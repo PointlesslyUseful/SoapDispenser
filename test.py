8348896736e5dc9a9dd61a0380d3c37ace8f2506
PointlesslyUseful <84037319+PointlesslyUseful@users.noreply.github.com>
2022-01-13 19:20:28 +0000

    remove junk

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import math
 from matplotlib import pyplot as plt
 import numpy as np
 
-img = cv2.imread('HaarTrain/train_hands/picture292.jpg')
+img = cv2.imread('HaarTrain/train_hands/picture281.jpg')
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
@@ -15,6 +15,22 @@ kernel = np.array([
 
 r = cv2.filter2D(gray, -1, kernel)
 
+h,w = r.shape
+
+annotated = gray = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+
+for column in range(w):
+    longest = 0
+    current = 0
+    for row in range(h):
+        if r[row][column] > 100:
+            current += 1
+        else:
+            if current > longest:
+                longest = current
+                current = 0
+
+    if current > 10:
 
 
 cv2.imwrite('test.jpg', r)
